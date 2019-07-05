35f45235af8b60b2fbbc793aaf78d66da489aab9
GeoProth <Gprothero@live.com>
2019-07-05 11:34:58 -0600

    Added more Queries

--- GroupProject/Items/clsItemsSQL.cs
+++ GroupProject/Items/clsItemsSQL.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using GroupProject.Classes;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupProject.Items
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class clsItemsSQL
     {
+        /// <summary>
+        /// Data Access Object
+        /// </summary>
+        clsDataAccess DataAccess;
+        /// <summary>
+        /// Data Set Object
+        /// </summary>
+        DataSet DS;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public clsMainSQL()
+        {
+            try
+            {
+                DataAccess = new clsDataAccess();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+
+        #region Get Queries
+        /// <summary>
+        /// Query to retrieve all the Items from Database
+        /// </summary>
+        /// <returns></returns>
+        public List<Item> GetAllItems()
+        {
+            try
+            {
+                List<Item> items = new List<Item>();
+
+                //Sql Statement
+                string Sql = "SELECT * FROM ItemDesc";
+
+                int retVal = 0;
+
+                DS = DataAccess.ExecuteSQLStatement(Sql, ref retVal);
+                //Loop through DS and add Items
+                foreach (DataRow dr in DS.Tables[0].Rows)
+                {
+                    items.Add(new Item(Convert.ToChar(dr[0]), dr[1].ToString(), (float)Convert.ToDouble(dr[2])));
+                }
+
+                return items;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        #endregion
     }
 }
--- GroupProject/Main/clsMainLogic.cs
+++ GroupProject/Main/clsMainLogic.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using GroupProject.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupProject.Main
 {
+    /// <summary>
+    /// Logic Class for Main Window
+    /// </summary>
     class clsMainLogic
     {
+        /// <summary>
+        /// SQL Object for main
+        /// </summary>
+        private clsMainSQL SQL;
+        /// <summary>
+        /// private list of Items
+        /// </summary>
+        private List<Item> items;
+        /// <summary>
+        /// current invoice
+        /// </summary>
+        private Invoice invoice;
+
+        public clsMainLogic()
+        {
+            try
+            {
+                SQL = new clsMainSQL();
+                //get all items
+                items = SQL.GetAllItems();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Getter and Setter for list of items
+        /// </summary>
+        public List<Item> ItemsList
+        {
+            get
+            {
+                return items;
+            }
+            set
+            {
+                this.items = value;
+            }
+        }
     }
 }
