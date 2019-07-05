92c0bae4d1ddf72ae5064a6afaaea527a3ce05d7
GeoProth <Gprothero@live.com>
2019-07-05 15:54:28 -0600

    added more queries

--- GroupProject/Search/clsSearchSQL.cs
+++ GroupProject/Search/clsSearchSQL.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using GroupProject.Classes;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,196 @@ namespace GroupProject.Search
 {
     class clsSearchSQL
     {
+        /// <summary>
+        /// Data Access Object
+        /// </summary>
+        clsDataAccess DataAccess;
+        /// <summary>
+        /// Data Set Object
+        /// </summary>
+        DataSet DS;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public clsSearchSQL()
+        {
+            try
+            {
+                DataAccess = new clsDataAccess();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+
+        #region Get Queries
+        /// <summary>
+        /// Get All Invoices
+        /// 
+        /// </summary>
+        /// <returns>A List of all Invoices in Database</returns>
+        public List<Invoice> GetAllInvoices()
+        {
+            try
+            {
+                List<Invoice> invoices = new List<Invoice>();
+                //Sql
+                string get = "SELECT * FROM Invoices";
+
+                int retVal = 0;
+                DS = DataAccess.ExecuteSQLStatement(get, ref retVal);
+
+                foreach(DataRow dr in DS.Tables[0].Rows)
+                {
+                    invoices.Add(new Invoice(Convert.ToInt32(dr[0]), dr[1].ToString()));
+                }
+
+                return invoices;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+
+
+        }
+        /// <summary>
+        /// Get list of Invoice Numbers for ComboBox
+        /// </summary>
+        /// <returns></returns>
+        public List<int> GetInvoiceNumbers()
+        {
+            try
+            {
+                List<int> numbers = new List<int>();
+                string get = "SELECT InvoiceNum FROM Invoices";
+
+                int retVal = 0;
+                DS = DataAccess.ExecuteSQLStatement(get, ref retVal);
+
+                foreach(DataRow dr in DS.Tables[0].Rows)
+                {
+                    numbers.Add(Convert.ToInt32(dr[0]));
+                }
+
+                return numbers;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Get Filtered Invoices based on filters passed
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <param name="invoiceDate"></param>
+        /// <param name="totalCost"></param>
+        /// <returns></returns>
+        public List<Invoice> GetFilteredInvoices(int invoiceNum, string invoiceDate, int totalCost)
+        {
+            try
+            {
+                List<Invoice> invoices = new List<Invoice>();
+                //partial Sql
+                string get = "SELECT * FROM Invoices WHERE ";
+                //decide what filter is being used
+                //if invoice Number is selected then its going to find a specific invoice, no need for date or cost
+                if(invoiceNum != -1)
+                {
+                    get += "InvoiceNum = " + invoiceNum;
+                }
+                // if Invoice date is selected
+                else if(invoiceDate != null)
+                {
+                    get += "InvoiceDate = " + invoiceDate;
+                    // if both invoice date and total cost are selected
+                    if(totalCost != -1)
+                    {
+                        get += "AND TotalCost = " + totalCost;
+                    }
+                }
+                //Assume its just total cost selected
+                else
+                {
+                    get += "TotalCost = " + totalCost;
+                }
+               
+
+                int retVal = 0;
+
+                DS = DataAccess.ExecuteSQLStatement(get, ref retVal);
+
+                foreach(DataRow dr in DS.Tables[0].Rows)
+                {
+                    invoices.Add(new Invoice(Convert.ToInt32(dr[0]), dr[1].ToString()));
+                }
+
+
+                return invoices;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Get a list of all Unique Dates from Invoices for Group box
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetUniqueDates()
+        {
+            try
+            {
+                List<string> dates = new List<string>();
+                string get = "SELECT DISTINCT InvoiceDate FROM Invoices";
+
+                int retVal = 0;
+
+                DS = DataAccess.ExecuteSQLStatement(get, ref retVal);
+
+                foreach(DataRow dr in DS.Tables[0].Rows)
+                {
+                    dates.Add(dr[0].ToString());
+                }
+
+                return dates;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Get all Unique Invoice totals for group box
+        /// </summary>
+        /// <returns></returns>
+        public List<double> GetUniqueTotals()
+        {
+            try
+            {
+                List<double> totals = new List<double>();
+                string get = "SELECT DISTINCT TotalCost FROM Invoices";
+
+                int retVal = 0;
+
+                DS = DataAccess.ExecuteSQLStatement(get, ref retVal);
+
+                foreach(DataRow dr in DS.Tables[0].Rows)
+                {
+                    totals.Add(Convert.ToDouble(dr[0]));
+                }
+
+                return totals;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " --> " + ex.Message);
+            }
+        }
+        #endregion
     }
 }
